021eeacec67474d0e365eb65aaa5dbcb41699be3
starhao <starhao@danwi.me>
2017-07-18 14:47:02 +0800

    对模块的一些注释

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ArticleComponent } from './article/article.component';
-import { ArticleEditComponent } from './article-edit/article-edit.component';
+import {AppComponent} from './app.component';
+import {ArticleComponent} from './article/article.component';
+import {ArticleEditComponent} from './article-edit/article-edit.component';
 
+/*NgModule 装饰器,接收一个用来描述模块属性的元数据对象.*/
 @NgModule({
-  declarations: [
-    AppComponent,
+  /*declarations 声明本模块中拥有的视图类.每新增一个模块, 都要在这里声明*/
+  declarations: [AppComponent,
     ArticleComponent,
-    ArticleEditComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+    ArticleEditComponent],
+
+  /*declaretions 的子集, 可用于其他模块的组件*/
+  exports: [],
+
+  /*本模块需要的其他模块*/
+  imports: [BrowserModule],
+
   providers: [],
+
+  /*指定主模块*/
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+/* AppModule是默认模块,导不导出都一样*/
+export class AppModule {
+}
